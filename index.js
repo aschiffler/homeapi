8298c5c77122def3639928886cdcbe213d1064e3
Andreas Schiffler <schiffler@roschi.eu>
2020-06-03 19:43:03 +0000

    function to join and un join

--- index.js
+++ index.js
@@ -7,41 +7,50 @@ const app = express();
 function verifyAccess(req, res, next) {
     let keyv = new Keyv('sqlite://database.sqlite');
     keyv.on('error', err => {
-        console.log('Connection to keyv store Error', err);
-        res.status(400);
+        console.log('[keyv] ' + err);
+        res.status(400).send('[keyv] ' + err);
     });
-    console.log('verify')
     if (req.body.Body == process.env.JOINSECRET)
     {
-        console.log('magicword')
-        keyv.get('validPhones')
+        keyv.set(req.body.From,true)
         .then((result) => {
-            console.log(result);
-            let newPhoneNumbers  = result.push(req.body.From);
-            keyv.set('validPhones',newPhoneNumbers).then((result) =>{
-                client.messages.create({from: 'whatsapp:' + process.env.botPhone, body: 'you are in', to: req.body.From}).then(message => console.log(req.body.From + ' has joined'));
+            client.messages.create({from: 'whatsapp:' + process.env.botPhone, body: 'you are in', to: req.body.From})
+            .then(message => {
+                console.log(req.body.From + ' has now access');
                 next();
-            })
+            });
         }, (reason) => {
-            console.log('[kevy reject] ' + reason)
+            console.log('[kevy] ' + reason)
+            res.status(200).send('keyv error');
+        });    
+    } else if (req.body.Body == 'noaccess'){
+        console.log('optout for ' + req.body.From)
+        keyv.set(req.body.From,false)
+        .then((result) => {
+            client.messages.create({from: 'whatsapp:' + process.env.botPhone, body: 'you are out', to: req.body.From})
+            .then(message => {
+                console.log(req.body.From + ' has now no access');
+                res.status(200).send('optout');
+            });
+        }, (reason) => {
+            console.log('[kevy] ' + reason)
             res.status(200).send('keyv error');
         });    
     } else {
-        keyv.get('validPhones')
+        keyv.get(req.body.From)
             .then((result) => {
-                if (1){
-                    console.log(req.body.From);
+                if (result == true){
                     next();
                 }else{
-                    client.messages.create({from: 'whatsapp:' + process.env.botPhone, body: 'you are in', to: req.body.From})
+                    client.messages.create({from: 'whatsapp:' + process.env.botPhone, body: 'you have no access. Tell me the magic word', to: req.body.From})
                         .then(message => {
                             console.log(req.body.From + ' has no access');
                             res.status(200).send('no access');
                         });
                 }
             }, (reason) => {
-                console.log('[kevy reject] ' + reason)
-                res.status(400);
+                console.log('[kevy] ' + reason)
+                res.status(400).send('[kevy] ' + reason);
             });
     }
 }
@@ -51,7 +60,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.post('/api', verifyAccess, (req, res) => {
-    console.log(req.body);
+    client.messages.create({from: 'whatsapp:' + process.env.botPhone, body: 'I do not undestand: ' + req.body.Body, to: req.body.From})
     res.status(200);
 });
 
